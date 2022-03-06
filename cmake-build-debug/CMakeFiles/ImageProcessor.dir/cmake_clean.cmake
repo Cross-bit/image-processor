41ce740fe14c06278a0db8534071935b0c97b9f8
Cross-bit <ondra.kryz@gmail.com>
2022-03-06 16:23:10 +0100

    working menu framework

---
+++ cmake-build-debug/CMakeFiles/ImageProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuCommands/GoIdleMenuOption.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuCommands/GoIdleMenuOption.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuCommands/GoSecondMenuOption.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuCommands/GoSecondMenuOption.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/IdleMenuGroup/IdleMenuGroup.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/IdleMenuGroup/IdleMenuGroup.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuGroup.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuGroup.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuGroupFactory.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuGroupFactory.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuOption.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/MenuOption.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/SecondMenuGroup/SecondMenuGroup.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/MenuGroups/SecondMenuGroup/SecondMenuGroup.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/UserMenu.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/UserMenu/UserMenu.cpp.obj.d"
+  "CMakeFiles/ImageProcessor.dir/main.cpp.obj"
+  "CMakeFiles/ImageProcessor.dir/main.cpp.obj.d"
+  "ImageProcessor.exe"
+  "ImageProcessor.exe.manifest"
+  "ImageProcessor.pdb"
+  "libImageProcessor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
