ec490647a38f5a9ae4b7d6e4e7c46226b08268eb
Cross-bit <ondra.kryz@gmail.com>
2022-03-08 23:16:10 +0100

    add of image library model class

---
+++ Imagesibrary/ImagesLibrary.h
@@ -0,0 +1,20 @@
+//
+// Created by kriz on 08.03.2022.
+//
+
+#ifndef MAIN_CPP_IMAGESLIBRARY_H
+#define MAIN_CPP_IMAGESLIBRARY_H
+
+#include <iostream>
+#include <vector>
+#include <vector>
+
+
+class ImagesLibrary {
+public:
+
+};
+
+
+#endif //MAIN_CPP_IMAGESLIBRARY_H
+
