80973b515ae8b863e128647d02d2d209ec63c832
Cross-bit <ondra.kryz@gmail.com>
2022-03-10 12:43:35 +0100

    image jpg type

---
+++ ImageLibrary/ImageFormat/ImageJPG.h
@@ -0,0 +1,18 @@
+//
+// Created by kriz on 10.03.2022.
+//
+
+#ifndef MAIN_CPP_IMAGEJPG_H
+#define MAIN_CPP_IMAGEJPG_H
+
+
+#include "../ImageFormat.h"
+
+class ImageJPG : ImageFormat {
+
+
+
+};
+
+
+#endif //MAIN_CPP_IMAGEJPG_H
