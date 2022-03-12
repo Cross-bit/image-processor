49c8e8acfd9df7eb29b8d7e226c21c32cc6d9903
Cross-bit <ondra.kryz@gmail.com>
2022-03-12 23:23:09 +0100

    implementation of static memebers

--- ImageLibrary/ImageFormat.cpp
+++ ImageLibrary/ImageFormat.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "ImageFormat.h"
+
+std::string ImageFormat::RemoveExstention(const std::string &fileName){
+    auto dotIndex = fileName.find_last_of( '.' );
+    return fileName.substr ( 0, dotIndex );
+}
+
+std::string ImageFormat::ParseFileName(const std::string &fileName) {
+
+    auto indexOfName = fileName.find_last_of("/\\");
+    return RemoveExstention(fileName.substr(indexOfName + 1 ));
+}
+
+
--- ImageLibrary/ImageFormat.h
+++ ImageLibrary/ImageFormat.h
@@ -13,10 +13,21 @@
 
 #include "Image/ImageData.h"
 
+
 class ImageFormat {
 public:
-    virtual ImageData LoadImageData(const std::string &inpFileName) const = 0;
+    virtual std::unique_ptr<ImageData> LoadImageData(const std::string &inpFileName) const = 0;
     virtual bool SaveImageData(const ImageData &dataToSave, const std::string &outFileName) const = 0;
+
+    static std::string RemoveExstention(const std::string &inpFileName);
+    static std::string ParseFileName(const std::string &inpFileName);
+
+    explicit ImageFormat()= default;
+    virtual ~ImageFormat()= default;
+
+protected:
+   // virtual std::string AddExstention(const std::string &inpFileName) const = 0;
+    //virtual std::string GetFormatExstention() const = 0;
 };
 
 
