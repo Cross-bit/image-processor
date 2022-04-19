63d987750a03c1c5046f63397a74d422efc2d0ba
Cross-bit <ondra.kryz@gmail.com>
2022-04-19 18:09:44 +0200

    added base class for image effect

---
+++ ImageEffects/ImageEffect.cpp
@@ -0,0 +1,7 @@
+//
+// Created by kriz on 19.04.2022.
+//
+
+#include "ImageEffect.h"
+
+ImageEffect::ImageEffect(ImageData &imageData) : _imageData(imageData) { }
---
+++ ImageEffects/ImageEffect.h
@@ -0,0 +1,21 @@
+//
+// Created by kriz on 19.04.2022.
+//
+
+#ifndef MAIN_CPP_IMAGEEFFECT_H
+#define MAIN_CPP_IMAGEEFFECT_H
+
+#include "../ImageLibrary/Image/ImageData.h"
+
+class ImageEffect {
+public:
+    ImageEffect(ImageData& imageData);
+
+    virtual void ApplyEffect() =0;
+
+private:
+    ImageData& _imageData;
+};
+
+
+#endif //MAIN_CPP_IMAGEEFFECT_H
