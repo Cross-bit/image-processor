d060a1d531f0a89d05f884d87f296f185a5ffa7a
Cross-bit <ondra.kryz@gmail.com>
2022-04-19 23:56:24 +0200

    Add Image effect facotry files

---
+++ ImageEffects/ImageEffectFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriz on 19.04.2022.
+//
+
+#include "ImageEffectFactory.h"
---
+++ ImageEffects/ImageEffectFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by kriz on 19.04.2022.
+//
+
+#ifndef MAIN_CPP_IMAGEEFFECTFACTORY_H
+#define MAIN_CPP_IMAGEEFFECTFACTORY_H
+
+
+class ImageEffectFactory {
+ // todo:
+};
+
+
+#endif //MAIN_CPP_IMAGEEFFECTFACTORY_H
