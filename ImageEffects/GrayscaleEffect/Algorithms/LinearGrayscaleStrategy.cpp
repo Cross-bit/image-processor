0c7b94dd7a7f1c2fa33da51ed340212715dc150e
Cross-bit <ondra.kryz@gmail.com>
2022-04-20 17:01:38 +0200

    Add files for linear grayscale strategies

---
+++ ImageEffects/GrayscaleEffect/Algorithms/LinearGrayscaleStrategy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriz on 20.04.2022.
+//
+
+#include "LinearGrayscaleStrategy.h"
---
+++ ImageEffects/GrayscaleEffect/Algorithms/LinearGrayscaleStrategy.h
@@ -0,0 +1,14 @@
+//
+// Created by kriz on 20.04.2022.
+//
+
+#ifndef MAIN_CPP_LINEARGRAYSCALESTRATEGY_H
+#define MAIN_CPP_LINEARGRAYSCALESTRATEGY_H
+
+
+class LinearGrayscaleStrategy {
+
+};
+
+
+#endif //MAIN_CPP_LINEARGRAYSCALESTRATEGY_H
