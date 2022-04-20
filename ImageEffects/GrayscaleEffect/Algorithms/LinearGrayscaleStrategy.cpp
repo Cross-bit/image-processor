9df824485c3d2a824c40c11517e8e5831926744a
Cross-bit <ondra.kryz@gmail.com>
2022-04-21 01:00:15 +0200

    Edit in implementation of linear grayscale alg.

--- ImageEffects/GrayscaleEffect/Algorithms/LinearGrayscaleStrategy.cpp
+++ ImageEffects/GrayscaleEffect/Algorithms/LinearGrayscaleStrategy.cpp
@@ -21,37 +21,15 @@ void LinearGrayscaleStrategy::SetLinearParameters(float red, float green, float 
     _blueCoef = blue;
 }
 
-float sRGBGammaExspansion(float value_srgb){
-    if(value_srgb <= 0.04045)
-        return value_srgb / 12.92;
-    else
-        return pow(((value_srgb+0.055)/1.055), 2.4);
-}
-
-float sRGBGammaCompression(float value_srgb){
-    if(value_srgb <= 0.0031308)
-        return value_srgb * 12.92;
-    else
-        return ((pow(value_srgb, 1/2.4)*1.055) - 0.055);
-}
-
 void LinearGrayscaleStrategy::TransformToGrayscale(ImageData &inputData) const {
 
     for (int i = 0; i < inputData.DataSize; i += inputData.ColorChannels) {
 
-
-        /*inputData.Data[i] = sRGBGammaCompression(inputData.Data[i] / (float) 255) * 255;
-        continue;*/
         float res;
-        res = _redCoef * sRGBGammaExspansion(inputData.Data[i] / (float) 255);
-        res += _greenCoef * sRGBGammaExspansion(inputData.Data[i + 1] / (float) 255);
-        res += _blueCoef * sRGBGammaExspansion(inputData.Data[i + 2] / (float) 255);
+        res = _redCoef * ImageData::sRGBGammaExspansion(inputData.Data[i] / (float) 255);
+        res += _greenCoef * ImageData::sRGBGammaExspansion(inputData.Data[i + 1] / (float) 255);
+        res += _blueCoef * ImageData::sRGBGammaExspansion(inputData.Data[i + 2] / (float) 255);
 
-        /*res = 0.2126 * inputData.Data[i];
-        res += 0.7152 * inputData.Data[i+1];
-        res += 0.0722 * inputData.Data[i+2];*/
-
-        //std::memset(&inputData.Data[i], res, inputData.ColorChannels);
-        std::memset(&inputData.Data[i], sRGBGammaCompression(res) * 255, inputData.ColorChannels);
+        std::memset(&inputData.Data[i], ImageData::sRGBGammaCompression(res) * 255, inputData.ColorChannels);
     }
 }
