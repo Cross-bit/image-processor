a460d32d40a27923a51ac8b052d365a5b22c1d6b
Cross-bit <ondra.kryz@gmail.com>
2022-04-21 19:12:59 +0200

    Added files for image kernel convoultion preparations

---
+++ ImageEffects/Convulention/ImageKernel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriz on 21.04.2022.
+//
+
+#include "ImageKernel.h"
---
+++ ImageEffects/Convulention/ImageKernel.h
@@ -0,0 +1,14 @@
+//
+// Created by kriz on 21.04.2022.
+//
+
+#ifndef MAIN_CPP_IMAGEKERNEL_H
+#define MAIN_CPP_IMAGEKERNEL_H
+
+
+class ImageKernel {
+
+};
+
+
+#endif //MAIN_CPP_IMAGEKERNEL_H
