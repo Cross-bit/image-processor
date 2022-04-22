25bc6d0fc6ca25f0ffb2415567cc327443c4a85a
Cross-bit <ondra.kryz@gmail.com>
2022-04-22 17:17:31 +0200

    Add files for ascii art effect

---
+++ ImageEffects/ASCII/AsciiArtEffect.h
@@ -0,0 +1,14 @@
+//
+// Created by kriz on 22.04.2022.
+//
+
+#ifndef MAIN_CPP_ASCIIARTEFFECT_H
+#define MAIN_CPP_ASCIIARTEFFECT_H
+
+
+class AsciiArtEffect {
+
+};
+
+
+#endif //MAIN_CPP_ASCIIARTEFFECT_H
