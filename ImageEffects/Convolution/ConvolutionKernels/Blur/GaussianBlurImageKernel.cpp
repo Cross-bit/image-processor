a92c1ec4c684b4281ba86bb78f150b2067b929aa
Cross-bit <ondra.kryz@gmail.com>
2022-04-23 22:09:58 +0200

    Added gaussian blur image effect

---
+++ ImageEffects/Convolution/ConvolutionKernels/Blur/GaussianBlurImageKernel.cpp
@@ -0,0 +1,61 @@
+//
+// Created by kriz on 23.04.2022.
+//
+
+#include "GaussianBlurImageKernel.h"
+GaussianBlurImageKernel::GaussianBlurImageKernel(int kernelDimension) : ImageKernel(kernelDimension)
+{
+    SetStandardDeviation(1);
+    PopulateKernelBufferWithValues();
+}
+
+GaussianBlurImageKernel::GaussianBlurImageKernel(int kernelDimension, double standardDiviation) : ImageKernel(kernelDimension) {
+    SetStandardDeviation(standardDiviation);
+    PopulateKernelBufferWithValues();
+}
+
+void GaussianBlurImageKernel::SetStandardDeviation(double value) {
+
+    _stdDiviation = value;
+    _precomputedDeviationCoef = 2 * pow(_stdDiviation, 2);
+}
+
+void GaussianBlurImageKernel::PopulateKernelBufferWithValues() {
+    // note: this algorithm works, but is far from optimal
+    // g. blur can be done in O(n)
+    // or even faster using box blur: http://blog.ivank.net/fastest-gaussian-blur.html
+
+    _kernelBuffer.clear();
+    double sum = 0;
+    for (int i = 0; i < _dimension; ++i) {
+        std::vector<double> bufferRow(_dimension);
+        for (int j = 0; j < _dimension; ++j) {
+            int x = i - (_dimension / 2);
+            int y = j - (_dimension / 2);
+
+            bufferRow[j] = Get2DGaussianValueOnCoords(x, y);
+            sum += bufferRow[j];
+        }
+        _kernelBuffer.emplace_back(std::move(bufferRow));
+    }
+
+    // normalize the kernel (so all sum up to 1)
+    for (int y = 0; y < _dimension; y++){
+        for (int x = 0; x < _dimension; x++){
+            _kernelBuffer[y][x] /= sum;
+        }
+    }
+}
+
+double GaussianBlurImageKernel::Get2DGaussianValueOnCoords(int x, int y) const{
+    // source: https://en.wikipedia.org/wiki/Gaussian_blur
+
+    double coordsComb = pow(x, 2) + pow(y, 2);
+
+    return 1/(exp(coordsComb/_precomputedDeviationCoef) * (M_PI*_precomputedDeviationCoef));
+}
+
+double GaussianBlurImageKernel::GetKernelValueOnCoords(int x, int y) const {
+    // return the corresponding kernel value
+   return _kernelBuffer[y][x];
+}
---
+++ ImageEffects/Convolution/ConvolutionKernels/Blur/GaussianBlurImageKernel.h
@@ -0,0 +1,51 @@
+//
+// Created by kriz on 23.04.2022.
+//
+
+#ifndef MAIN_CPP_GAUSSIANBLURIMAGEKERNEL_H
+#define MAIN_CPP_GAUSSIANBLURIMAGEKERNEL_H
+
+#include "../ImageKernel.h"
+#include <vector>
+class GaussianBlurImageKernel : public ImageKernel
+{
+
+public:
+    GaussianBlurImageKernel(int kernelDimension);
+    GaussianBlurImageKernel(int kernelDimension, double standardDiviation);
+
+    double GetKernelValueOnCoords(int x, int y) const override;
+
+    /**
+     * Calculates kernel matrix values and sets internal buffer.
+     * @return
+     */
+    void PopulateKernelBufferWithValues();
+
+    /**
+     * Setter for standard deviation.
+     */
+    void SetStandardDeviation(double value);
+
+private:
+
+    double Get2DGaussianValueOnCoords(int x, int y) const;
+
+    /**
+     * Kernel buffer matrix.
+     */
+    std::vector<std::vector<double>>  _kernelBuffer;
+
+    /**
+     * Gaussian distribution standard diviation.
+     */
+    double _stdDiviation;
+    /**
+     * Coefitient based on standard diviation used in the formula. (Is recalculated with setting std diviation)
+     */
+    double _precomputedDeviationCoef;
+
+};
+
+
+#endif //MAIN_CPP_GAUSSIANBLURIMAGEKERNEL_H
