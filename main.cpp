7983779c145ad019e9c3e14ac2549938a686faec
Cross-bit <ondra.kryz@gmail.com>
2022-04-25 18:46:49 +0200

    Better testing in main

--- main.cpp
+++ main.cpp
@@ -20,17 +20,23 @@
 
 #include "ImageEffects/Convolution/ConvolutionKernels/Blur/BoxBlurImageKernel.h"
 #include "ImageEffects/Convolution/ConvolutionKernels/Blur/GaussianBlurImageKernel.h"
-#include "ImageEffects/Convolution/ConvolutionKernels/EdgeDetection/SobelImageKernel.h"
+#include "ImageEffects/Convolution/ConvolutionKernels/Sobel/SobelImageKernel.h"
 
 #include "ImageEffects/ASCII/AsciiArtEffect.h"
 
+#include "ImageEffects/ImageEffectFactory.h"
+
 #include <cstdio>
+ void tmpStoreImg(ImageData& data, const ImageFormat& format, const std::string& name){
+    data.Name = data.Name + "_" + name;
+    format.SaveImageData(data, "../TestRes");
+}
 
 int main() {
 
     ImageJPG jpgFormat;
 
-    std::string file = "../Resources/leaf.jpg";
+    std::string file = "../Resources/harysek_2.jpg";
 
     auto res = jpgFormat.LoadImageData(file);
     if(res!= nullptr){
@@ -42,69 +48,21 @@ int main() {
     else
         std::cout << "does not work" << std::endl;
 
+    ImageEffectFactory imageFactory(*res);
 
-    std::unique_ptr<GrayscaleStrategyBase> strategyBase = std::make_unique<LinearGrayscaleStrategy>();// .2126, .7152, .0722
-    strategyBase->TransformToGrayscale(*res);
+    //LinearGrayscaleStrategy algorithm;
+    //auto effect = imageFactory.CreateLinearGrayScale();// grayscale linear
 
-    res->Name = res->Name + "_n";
+    /*std::string inputAlpha = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,¨^`'. ";
+    inputAlpha = "$@B%8&WM#;:,¨^`'. ";
 
-    jpgFormat.SaveImageData(*res, "../Resources");
-    //auto effect = new GrayscaleEffect(*res, *strategyBase);
+    auto effect = imageFactory.CreateAsciiArtEffectByScale(inputAlpha, 100, 0.65, std::cout); // grayscale avg*/
+   // auto effect = imageFactory.CreateBoxBlurConvolution(); // box blur
+   // auto effect = imageFactory.CreateGaussianBlurConvolution(11, 11); // gaussian blur
+    auto effect = imageFactory.CreateColorInversion(1);
 
-    /*auto effect = new NegativeEffect(*res, 0.5);
-    effect->ApplyEffect();*/
-    printf("img h: %d\n", res->Height);
-    printf("ref: %d\n", res->Data[res->DataSize - 4]);
-    //std::cout << "reference: " << unsigned (res->Data[2]);
-
-   // std::string inputAlpha = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,¨^`'. ";
-    std::string inputAlpha = "$@B%8&WM#;:,¨^`'. ";
-
-    //reverse(inputAlpha.begin(), inputAlpha.end());
-   // std::string inputAlpha = "abc";
-    auto asciiTest = new AsciiArtEffect(*res, inputAlpha, 100, 0.5f, std::cout);
-    asciiTest->ProcessImageData();
-
-    return 0;
-
-    auto radius = 21;
-
-    double stdDeviation = sqrt(-(radius^2) / (2*std::log10(1 /(float) 255)));
-    std::cout << stdDeviation;
-
-    //auto imgKernelTest = new GaussianBlurImageKernel(radius, 15);
-    auto imgKernelTest = new SobelImageKernel(SobelImageKernel::Direction::LEFT);
-    auto convolutionProcessor = new ConvolutionProcessor(*res, *imgKernelTest, false);
-
-    convolutionProcessor->ProcessImageData();
-
-    auto& filtered = convolutionProcessor->GetConvolutedImageData();
-
-    /*for (int i = 0; i < 15; ++i) {
-        auto convolutionProcessora = new ConvolutionProcessor(filtered, *imgKernelTest);
-        convolutionProcessora->ProcessImageData();
-        filtered = convolutionProcessora->GetConvolutedImageData();
-    }*/
-
-    filtered.Name = filtered.Name + "_c_s";
-    jpgFormat.SaveImageData(filtered, "../Resources");
-
-    return 0;
-    /*for (int i = 0; i < res->DataSize; ++i) {
-        std::cout << unsigned(res->Data[i]) << " ";
-    }*/
-    
-    return 0;
-
-
-
-    // set new file name:
-    res->Name = res->Name + "_n";
-
-    jpgFormat.SaveImageData(*res, "../Resources");
-
-    /*UserMenu userMenu;
-    userMenu.Update();*/
+    effect->ProcessImageData();
+    tmpStoreImg(effect->GetProcessedImageData(), jpgFormat, "i");
 
     return 0;
 }
