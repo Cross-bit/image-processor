819612c6f7d69280d266ae1a328a140a527cae59
Cross-bit <ondra.kryz@gmail.com>
2022-04-26 22:37:20 +0200

    added option menu group option to set default data load dir

---
+++ UserMenu/MenuGroups/OptionsMenuGroup/Commands/SetDefaultInputDirectory.h
@@ -0,0 +1,20 @@
+//
+// Created by kriz on 26.04.2022.
+//
+
+#ifndef MAIN_CPP_SETDEFAULTINPUTDIRECTORY_H
+#define MAIN_CPP_SETDEFAULTINPUTDIRECTORY_H
+
+
+#include "../../../MenuCommands/MenuOption.h"
+
+class SetDefaultInputDirectory : MenuOption
+{
+public:
+    SetDefaultInputDirectory();
+
+    void Execute() override;
+};
+
+
+#endif //MAIN_CPP_SETDEFAULTINPUTDIRECTORY_H
