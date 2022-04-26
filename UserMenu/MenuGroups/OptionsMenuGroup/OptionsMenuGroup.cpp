cec49e66026d982de3cf0c0251e6cb292a70280b
Cross-bit <ondra.kryz@gmail.com>
2022-04-26 22:15:20 +0200

    Add options menu group

---
+++ UserMenu/MenuGroups/OptionsMenuGroup/OptionsMenuGroup.cpp
@@ -0,0 +1,9 @@
+//
+// Created by kriz on 26.04.2022.
+//
+
+#include "OptionsMenuGroup.h"
+
+OptionsMenuGroup::OptionsMenuGroup() {
+    _headerData = "Configuration menu";
+}
---
+++ UserMenu/MenuGroups/OptionsMenuGroup/OptionsMenuGroup.h
@@ -0,0 +1,17 @@
+//
+// Created by kriz on 26.04.2022.
+//
+
+#ifndef MAIN_CPP_OPTIONSMENUGROUP_H
+#define MAIN_CPP_OPTIONSMENUGROUP_H
+
+
+#include "../MenuGroup.h"
+
+class OptionsMenuGroup : public MenuGroup {
+public:
+    OptionsMenuGroup();
+
+};
+
+#endif //MAIN_CPP_OPTIONSMENUGROUP_H
