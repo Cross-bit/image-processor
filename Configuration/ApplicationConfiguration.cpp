62c47636ed550fd50f6c191435271a80106e14f6
Cross-bit <ondra.kryz@gmail.com>
2022-04-26 22:35:44 +0200

    Added configuration file class

---
+++ Configuration/ApplicationConfiguration.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriz on 26.04.2022.
+//
+
+#include "ApplicationConfiguration.h"
---
+++ Configuration/ApplicationConfiguration.h
@@ -0,0 +1,20 @@
+//
+// Created by kriz on 26.04.2022.
+//
+
+#ifndef MAIN_CPP_APPLICATIONCONFIGURATION_H
+#define MAIN_CPP_APPLICATIONCONFIGURATION_H
+
+#include <iostream>
+
+class ApplicationConfiguration {
+public:
+    ApplicationConfiguration();
+
+    std::string DefaultInputDir;
+    std::string DefaultOutputDir;
+
+};
+
+
+#endif //MAIN_CPP_APPLICATIONCONFIGURATION_H
