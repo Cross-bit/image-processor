2d83b57930566f2d65f38111d3425d198c0c3c4c
Cross-bit <ondra.kryz@gmail.com>
2022-04-27 16:06:16 +0200

    Added menu option for storing image data

---
+++ UserMenu/GlobalMenuCommands/StoreImageDataOption.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriz on 27.04.2022.
+//
+
+#include "StoreImageDataOption.h"
---
+++ UserMenu/GlobalMenuCommands/StoreImageDataOption.h
@@ -0,0 +1,14 @@
+//
+// Created by kriz on 27.04.2022.
+//
+
+#ifndef MAIN_CPP_STOREIMAGEDATAOPTION_H
+#define MAIN_CPP_STOREIMAGEDATAOPTION_H
+
+
+class StoreImageDataOption {
+
+};
+
+
+#endif //MAIN_CPP_STOREIMAGEDATAOPTION_H
